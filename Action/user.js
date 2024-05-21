fa86485a206ec1b7b4bea150ebfd28fd8f7ac7bb
ArthurKhalifa <arthursantosae@hotmail.com>
2024-05-21 12:13:00 -0300

    Upload and store image

--- Action/user.js
+++ Action/user.js
@@ -2,60 +2,52 @@ import { db } from "../db.js";
 
 export const getUsers = (_, res) => {
     const q = "SELECT * FROM usuarios";
-
     db.query(q, (err, data) => {
         if (err) return res.json(err);
-
         return res.status(200).json(data);
     });
 };
 
 export const addUser = (req, res) => {
-    const q =
-        "INSERT INTO usuarios(`nome`, `uf`, `email`, `fone`) VALUES(?)";
-
+    const q = "INSERT INTO usuarios(`nome`, `idade`, `rua`, `bairro`, `estado`, `biografia`, `imagem`) VALUES(?)";
     const values = [
         req.body.nome,
-        req.body.uf,
-        req.body.email,
-        req.body.fone,
+        req.body.idade,
+        req.body.rua,
+        req.body.bairro,
+        req.body.estado,
+        req.body.biografia,
+        req.file ? `/public/images/${req.file.filename}` : null
     ];
 
     db.query(q, [values], (err) => {
         if (err) return res.json(err);
-
         return res.status(200).json("Usuário criado com sucesso.");
     });
 };
 
 export const updateUser = (req, res) => {
-    const q =
-        "UPDATE usuarios SET `nome` = ?, `uf` = ?, `email` = ?,  `fone` = ? WHERE `id` = ?";
-
+    const q = "UPDATE usuarios SET `nome` = ?, `idade` = ?, `rua` = ?, `bairro` = ?, `estado` = ?, `biografia` = ?, `imagem` = ? WHERE `id` = ?";
     const values = [
         req.body.nome,
-        req.body.uf,
-        req.body.email,
-        req.body.fone,
+        req.body.idade,
+        req.body.rua,
+        req.body.bairro,
+        req.body.estado,
+        req.body.biografia,
+        req.file ? `/public/images/${req.file.filename}` : req.body.imagem
     ];
-
 
     db.query(q, [...values, req.params.id], (err) => {
         if (err) return res.json(err);
-
         return res.status(200).json("Usuário atualizado com sucesso.");
     });
 };
 
 export const deleteUser = (req, res) => {
     const q = "DELETE FROM usuarios WHERE `id` = ?";
-
     db.query(q, [req.params.id], (err) => {
         if (err) return res.json(err);
-
         return res.status(200).json("Usuário deletado com sucesso.");
     });
 };
-
-
-
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,36 @@
-import express from "express"
+import express from "express";
+import cors from 'cors';
+import multer from 'multer';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import { addUser, deleteUser, getUsers, updateUser } from "../Action/user.js";
 
-const router = express.Router()
-router.get("/", getUsers)
+const nameFile = fileURLToPath(import.meta.url);
+const directName = path.dirname(nameFile);
 
-router.post("/", addUser)
+const router = express.Router();
+router.use(express.json());
+router.use(cors());
+router.use('/public/images', express.static(path.join(directName, '/public/images')));
 
-router.put("/:id", updateUser)
+// UPLOAD
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(directName, '/public/images'));
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname));
+    }
+});
 
-router.delete("/:id", deleteUser)
+const upload = multer({
+    storage: storage
+}).single('imagem');
 
-export default router
+router.get("/", getUsers);
+router.post("/", upload, addUser);
+router.put("/:id", upload, updateUser);
+router.delete("/:id", deleteUser);
+
+export default router;
